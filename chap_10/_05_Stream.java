d6986f22e590c3e0edb4bca196d4df7827355805
장정우 <chiang324@mocomsys.com>
2023-09-13 15:50:45 +0900

    나도코딩 섹션10 완료

--- chap_10/_05_Stream.java
+++ chap_10/_05_Stream.java
@@ -4,20 +4,24 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class _05_Stream {
     public static void main(String[] args) {
         //스트림 : 데이터가 흐른다
+        //Stream 은 한번 생성하면 다음에 사용할 수 없음
+        // 새로 만들어 주셔야합니다.
+
         int[] scores = {100, 95, 90, 85, 80};
         IntStream scoreStream = Arrays.stream(scores);
 
-        String[] langs = "Python,Java,Javascript,c,c++,c#".split(",");
+        String[] langs = "Python,java,javascript,c,c++,c#".split(",");
         Stream<String> stringStream = Arrays.stream(langs);
 
         List<String> langList = new ArrayList<>();
-        langList = Arrays.asList("Python,Java,Javascript,c,c++,c#".split(","));
+        langList = Arrays.asList("Python,java,javascript,c,c++,c#".split(","));
 
 //        System.out.println(langList.size());
         Stream<String> langListStream = langList.stream();
@@ -27,6 +31,7 @@ public class _05_Stream {
         //스트림 사용
 
         //중간 연산 (Intermediate Operation) : filter, map, sorted, distinct, skip
+        //최종 연산 (Intermediate Operation) : count, min, max, sum, forEach, anyMatch, allMatch....
 
         // 90점 이상인 점수만 출력
         Arrays.stream(scores).filter(x -> x>= 90).forEach(x-> System.out.println(x));
@@ -49,11 +54,58 @@ public class _05_Stream {
 //        95
 //        100
 
-        //최종 연산 (Intermediate Operation) : count, min, max, sum, forEach, anyMatch, allMatch....
 
+        //문자열 처리
+        // "python", "java", "javascript"
+        Arrays.stream(langs).filter(x -> x.startsWith("c")).forEach(System.out::println);
+        System.out.println("==================================================");
+//        c
+//        c++
+//        c#
 
+        //java라는 글자를 포함하는 언어
+        Arrays.stream(langs).filter(x-> x.contains("java")).forEach(System.out::println);
+        System.out.println("==================================================");
 
+        //글자 길이가 4 이하
+        langList.stream().filter(x-> x.length() <=4).sorted().forEach(System.out::println);
+        System.out.println("==================================================");
+//        c
+//        c#
+//        c++
+//        java
 
+        langList.stream().filter(x-> x.length() <=4 ).filter(x -> x.contains("c")).forEach(System.out::println);
+        System.out.println("==================================================");
+
+        boolean anyMatch = langList.stream()
+                .filter(x -> x.length() <= 4)
+                .anyMatch(x -> x.contains("c"));
+
+        System.out.println(anyMatch);
+        System.out.println("==================================================");
+
+        //4글자 이하의 언어중 C라는 글자를 포함하는 언어 뒤에 어려워요 라는 글자를 함께 출력
+        langList.stream()
+                .filter(x -> x.length() <=4)
+                .filter(x->x.contains("c"))
+                .map(x-> x+"(어려웡)").forEach(System.out::println);
+
+        System.out.println("==================================================");
+        //대문자로 변경
+        langList.stream()
+                .filter(x->x.contains("c"))
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+
+        System.out.println("==================================================");
+        //c 라는 글자를 포함하는 언어를 대문자로 변경하고 리스트에 저장
+         List<String> lsit = langList.stream()
+                .filter(x->x.contains("c"))
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+         lsit.stream().forEach(System.out::println);
 
     }
 }
---
+++ chap_10/_Quiz_10.java
@@ -0,0 +1,52 @@
+package chap_10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class _Quiz_10 {
+    public static void main(String[] args) {
+
+        List<Customer> customers = new ArrayList<>();
+
+        customers.add(new Customer("챈들러", 50));
+        customers.add(new Customer("레이첼", 42));
+        customers.add(new Customer("모니카", 21));
+        customers.add(new Customer("벤자민", 18));
+        customers.add(new Customer("제임스", 5));
+
+        System.out.println("미술관 입장료");
+        System.out.println("=============================");
+        customers.stream()
+                .map(x -> x.age >20? x.name+" 5000원" : x.name+" 무료." )
+                .forEach(System.out::println);
+    }
+}
+
+class Customer{
+    public String name;
+    public int age;
+
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
